bfc611d89c2565f1eb87f0239df02e92a4a61b9b
JulianCamachoCalle <camachocallejulian@gmail.com>
2025-05-10 16:57:29 -0500

    feat: Update application configuration with JWT support and modify routing for authentication

--- frontend-neuroplay/src/app/app.component.ts
+++ frontend-neuroplay/src/app/app.component.ts
@@ -2,14 +2,25 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms'
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptorService } from './services/auth/jwt-interceptor.service';
+import { ErrorInterceptorService } from './services/auth/error-interceptor.service';
+import { AuthInterceptorService } from './services/auth/auth-interceptor.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptorService, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true }
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend-neuroplay';
+  title = 'Neuroplay';
+
+  constructor() { }
 }
